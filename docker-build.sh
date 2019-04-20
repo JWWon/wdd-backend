ad7f048d13a6e5446a0d558854e33e10c8997ea1
JWWon <wonjiwn@naver.com>
2019-04-20 14:49:42 +0900

    Update docker-build.sh & .dockerignore

--- docker-build.sh
+++ docker-build.sh
@@ -2,7 +2,7 @@ AWS_ACCESS_KEY_ID=$(aws --profile default configure get aws_access_key_id)
 AWS_SECRET_ACCESS_KEY=$(aws --profile default configure get aws_secret_access_key)
 
 docker build -t woodongdang/backend .
-docker run -p 80:80 -d \
+docker run -it --name backend -p 80:80 -d \
   -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
   -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
   woodongdang/backend
