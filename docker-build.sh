0344cf0dbfab0ec89e0a1490fffa4debe085cc6d
JWWon <wonjiwn@naver.com>
2019-04-20 15:02:08 +0900

    rollback docker-build

--- docker-build.sh
+++ docker-build.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+docker build -t wdd-backend:latest .
+
 AWS_ACCESS_KEY_ID=$(aws --profile default configure get aws_access_key_id)
 AWS_SECRET_ACCESS_KEY=$(aws --profile default configure get aws_secret_access_key)
 
-docker build -t woodongdang/backend .
-docker run -it --name backend -p 80:80 -d \
+docker create --name wdd-backend -p 80:80 \
   -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
   -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-  woodongdang/backend
+  wdd-backend:latest
